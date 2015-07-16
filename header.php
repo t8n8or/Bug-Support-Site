3be13c4b4fc0f886f33e335071c43ab1d1543de5
t8n8or <tate@tmphotography.co.nz>
2015-07-16 12:52:59 +1200

    header formating

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <?
-require_once('authenticator.php');
-$authenticator = new AuthenticatorHelper();
+//require_once('authenticator.php');
+//$authenticator = new AuthenticatorHelper();
 ?>
 <html>
 <head>
-    <title>Shame-n-ator!</title>
+    <title>Bug Support</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="/shaming9000/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="/main.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
@@ -16,30 +16,29 @@ $authenticator = new AuthenticatorHelper();
 <body>
   <div class="container main">
     <div class="row">
-      <? if( $authenticator->isAuthenticated() ): ?>
+      <? //if( $authenticator->isAuthenticated() ): ?>
           <ul class="col-xs-3">
-              <li><a href="/shaming9000/admin.php?action=manage-shames">Manage Shame</a></li>
-              <li><a href="/shaming9000/admin.php?action=manage-websites">Manage Websites</a></li>
-              <li><a href="/shaming9000/admin.php?action=manage-users">Manage Users</a></li>
-              <li><a href="/shaming9000/index.php?logout=yes">Logout</a></li>
+              <li><a href="/ios/">iOS</a></li>
+              <li><a href="/android/">Android</a></li>
+              <li><a href="/home.php?logout=yes">Logout</a></li>
           </ul>
-      <? else: ?>
+      <? //else: ?>
           <form class="col-xs-3 loginform" method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
-              <h3>Shaming 9000</h3>
-              <br>
+              <h3>Bug Support</h3>
+              <br />
 
               <input type="text" name="login[username]" placeholder="Username"/>
-              <br>
-              <br>
+              <br />
+              <br />
              
               <input type="password" name="login[password]" placeholder="Password"/>
               
-              <br>
-              <br>
+              <br />
+              <br />
               <button class="btn btn-primary">Login</button>
-              <br>
-              <br>
+              <br />
+              <br />
           </form>
-      <? endif; ?>
+      <? //endif; ?>
       
       <div class="col-xs-9">
